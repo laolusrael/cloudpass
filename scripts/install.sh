4dd3a5209c07b99ca567b8377f81ae5c033c3b76
Seun S. Lawal <laolusrael@gmail.com>
2026-04-03 20:12:51 +0100

    fix(install): remove local from non-function context
    - Remove 'local' from line that was moved outside function

--- scripts/install.sh
+++ scripts/install.sh
@@ -277,7 +277,7 @@ info "Copying files..."
 cp "$SOURCE_DIR/cloudpass" "$INSTALL_DIR/"
 cp "$SOURCE_DIR/config.yaml" "$INSTALL_DIR/"
 
-local multipass_type
+multipass_type
 multipass_type=$(detect_multipass_type)
 get_multipass_paths "$multipass_type"
 
